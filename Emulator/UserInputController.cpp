fef35e98c6a7d24e659c87ed63cf71139bffd450
Víctor M. González <helios.vmg@gmail.com>
2016-09-28 02:55:55 -0300

    Fixed bug in UserInputController.

--- Emulator/UserInputController.cpp
+++ Emulator/UserInputController.cpp
@@ -8,16 +8,17 @@ UserInputController::UserInputController(Gameboy &system)
 
 void UserInputController::request_input_state(byte_t select){
 	this->saved_state = 0;
-	if (select & pin14_mask){
+	if (!(select & pin14_mask)){
 		this->saved_state |= this->input_state.right & pin10_mask;
 		this->saved_state |= this->input_state.left & pin11_mask;
 		this->saved_state |= this->input_state.up & pin12_mask;
 		this->saved_state |= this->input_state.down & pin13_mask;
 	}
-	if (select & pin15_mask){
+	if (!(select & pin15_mask)){
 		this->saved_state |= this->input_state.a & pin10_mask;
 		this->saved_state |= this->input_state.b & pin11_mask;
 		this->saved_state |= this->input_state.select & pin12_mask;
 		this->saved_state |= this->input_state.start & pin13_mask;
 	}
+	this->saved_state |= 0xC0;
 }
